60b7dcc9d8fcfc49cba3f8fc281357993389f452
KirillPopov001 <55420132+KirillPopov001@users.noreply.github.com>
2019-10-14 22:03:11 +0700

    Update case.py
    нормич

--- case.py
+++ case.py
@@ -15,8 +15,10 @@ for month in range(12):
 print(annual_income)
 
 
-def sub():
-    if annual_income <= 9075:
+def couple():
+    if annual_income <= 18150:
+        taxes = 0.1
+        s = 1
         gr1 = annual_income * 0.1
         gr2 = 0
         gr3 = 0
@@ -24,80 +26,67 @@ def sub():
         gr5 = 0
         gr6 = 0
         gr7 = 0
-    elif 9076 <= annual_income <= 36900:
-        gr1 = 9075 * 0.1
-        gr2 = (annual_income - 9075) * 0.15
+    elif 18151 <= annual_income <= 73800:
+        taxes = 0.15
+        s = 2
+        gr1 = 18150 * 0.1
+        gr2 = (annual_income - 18150) * 0.15
         gr3 = 0
         gr4 = 0
         gr5 = 0
         gr6 = 0
         gr7 = 0
-    elif 36901 <= annual_income <= 89350:
-        gr1 = 9075 * 0.1
-        gr2 = (36900 - 9075) * 0.15
-        gr3 = (annual_income - 36900) * 0.25
+    elif 73801 <= annual_income <= 148850:
+        taxes = 0.25
+        s = 3
+        gr1 = 18150 * 0.1
+        gr2 = (73800 - 18150) * 0.15
+        gr3 = (annual_income - 73800) * 0.25
         gr4 = 0
         gr5 = 0
         gr6 = 0
         gr7 = 0
-    elif 89351 <= annual_income <= 186350:
-        gr1 = 9075 * 0.1
-        gr2 = (36900 - 9075) * 0.15
-        gr3 = (89350 - 36900) * 0.25
-        gr4 = (annual_income - 89350) * 0.28
-        gr5 = 0
-        gr6 = 0
-        gr7 = 0
-    elif 186351 <= annual_income <= 405100:
-        gr1 = 9075 * 0.1
-        gr2 = (36900 - 9075) * 0.15
-        gr3 = (89350 - 36900) * 0.25
-        gr4 = (186350 - 89350) * 0.28
-        gr5 = (annual_income - 186350) * 0.33
-        gr6 = 0
-        gr7 = 0
-    elif 405101 <= annual_income <= 406750:
-        gr1 = 9075 * 0.1
-        gr2 = (36900 - 9075) * 0.15
-        gr3 = (89350 - 36900) * 0.25
-        gr4 = (186350 - 89350) * 0.28
-        gr5 = (405100 - 186350) * 0.33
-        gr6 = (annual_income - 405100) * 0.35
-        gr7 = 0
-    elif 406751 < annual_income:
-        gr1 = 9075 * 0.1
-        gr2 = (36900 - 9075) * 0.15
-        gr3 = (89350 - 36900) * 0.25
-        gr4 = (186350 - 89350) * 0.28
-        gr5 = (405100 - 186350) * 0.33
-        gr6 = (406750 - 405100) * 0.35
-        gr7 = (annual_income - 406750) * 0.396
-    print(gr1 + gr2 + gr3 + gr4 + gr5 + gr6 + gr7)
-    pass
-
-
-def couple():
-    if annual_income <= 18150:
-        taxes = 0.1
-        s = 1
-    elif 18151 <= annual_income <= 73800:
-        taxes = 0.15
-        s = 2
-    elif 73801 <= annual_income <= 148850:
-        taxes = 0.25
-        s = 3
     elif 148851 <= annual_income <= 226850:
         taxes = 0.28
         s = 4
+        gr1 = 18150 * 0.1
+        gr2 = (73800 - 18150) * 0.15
+        gr3 = (148850 - 73800) * 0.25
+        gr4 = (annual_income - 148850) * 0.28
+        gr5 = 0
+        gr6 = 0
+        gr7 = 0
     elif 226851 <= annual_income <= 405100:
         taxes = 0.33
         s = 5
+        gr1 = 18150 * 0.1
+        gr2 = (73800 - 18150) * 0.15
+        gr3 = (148850 - 73801) * 0.25
+        gr4 = (226850 - 148851) * 0.28
+        gr5 = (annual_income - 226850) * 0.33
+        gr6 = 0
+        gr7 = 0
     elif 405101 <= annual_income <= 457600:
         taxes = 0.35
         s = 6
+        gr1 = 18150 * 0.1
+        gr2 = (73800 - 18150) * 0.15
+        gr3 = (148850 - 73800) * 0.25
+        gr4 = (226850 - 148850) * 0.28
+        gr5 = (405100 - 226850) * 0.33
+        gr6 = (annual_income - 405100) * 0.35
+        gr7 = 0
     elif 457601 <= annual_income:
         taxes = 0.396
         s = 7
+        gr1 = 18150 * 0.1
+        gr2 = (73800 - 18150) * 0.15
+        gr3 = (148850 - 73800) * 0.25
+        gr4 = (226850 - 148850) * 0.28
+        gr5 = (405100 - 226850) * 0.33
+        gr6 = (457600 - 405100) * 0.35
+        gr7 = (annual_income - 457600) * 0.396
+    print(gr1 + gr2 + gr3 + gr4 + gr5 + gr6 + gr7)
     pass
 
 
